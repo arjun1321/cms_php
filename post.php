8a024393d6f8c7fcb8a7cdc76e986ca20d041de2
arjun1321 <guptaarjun152@gmail.com>
2016-08-01 14:06:43 +0530

    ui bug fixes

--- post.php
+++ post.php
@@ -16,6 +16,8 @@
 
     <!-- Custom CSS -->
     <link href="css/blog-post.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="css/style.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
