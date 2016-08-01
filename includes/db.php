a2aacbfe7382b65049a5dc9278a09ffc9c632d7e
arjun1321 <guptaarjun152@gmail.com>
2016-08-01 15:03:11 +0530

    inserting data into category table and displaying it

--- includes/db.php
+++ includes/db.php
@@ -14,10 +14,10 @@ foreach($db as $key => $value) {
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-if($connection) {
-    
-    echo "We are connected";
-}
+//if($connection) {
+//    
+//    echo "We are connected";
+//}
 
 
 ?>
