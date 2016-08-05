a422cbd0a4d36545c12852be26b9dc88c9222886
arjun1321 <guptaarjun152@gmail.com>
2016-08-05 15:07:53 +0530

    creating a custom search engine

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,18 +1,31 @@
 <div class="col-md-4">
+                
+       
+                
+                
 
                 <!-- Blog Search Well -->
                 <div class="well">
                     <h4>Blog Search</h4>
+                    <form action="search.php" method="post">
+                        
+                    
                     <div class="input-group">
-                        <input type="text" class="form-control">
+                        <input type="text" class="form-control" name="search">
                         <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
+                            <button class="btn btn-default" type="submit" name="submit">
                                 <span class="glyphicon glyphicon-search"></span>
                         </button>
                         </span>
                     </div>
+                    </form><!-- search form -->
                     <!-- /.input-group -->
                 </div>
+                
+                
+                
+                
+                
 
                 <!-- Blog Categories Well -->
                 <div class="well">
