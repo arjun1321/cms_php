f3d01520ff318d1f8a585351170a8e64290eac52
arjun1321 <guptaarjun152@gmail.com>
2016-08-16 15:04:30 +0530

    Adding categories

--- admin/categories.php
+++ admin/categories.php
@@ -19,8 +19,35 @@
                
                    
                          <div class="col-xs-6">
+                            
+<?php 
                              
-                             <form action="">
+    if(isset($_POST['submit'])) {
+
+        $cat_title = $_POST['cat_title'];
+        
+        if($cat_title == "" || empty($cat_title)) {
+            
+            echo "This field should not be empty";
+        } else {
+            
+            $query = "INSERT INTO categories(cat_title)";
+            $query .= "VALUE('{$cat_title}')";
+            
+            $create_categry_query = mysqli_query($connection, $query);
+            
+            if(!$create_categry_query) {
+
+                die('QUERY FAILED' . mysqli_error($connection));
+            }
+        }
+    }
+?>  
+                            
+                            
+                            
+                             
+                             <form action="" method="post">
                                  
                                  
                                  <div class="form-group">
