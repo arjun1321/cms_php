fbfad3e6bb92684f840dfedaf128b7f1d630cc05
arjun1321 <guptaarjun152@gmail.com>
2016-08-16 15:10:00 +0530

    Adding special function to header file

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,5 +1,19 @@
 <?php include "../includes/db.php" ?>
 
+<?php ob_start(); ?>
+
+
+
+
+<?php session_start(); ?>
+
+<?php 
+
+    if(!isset($_SESSION['user_role'])) {
+        
+        header("Location: ../index.php ");
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
