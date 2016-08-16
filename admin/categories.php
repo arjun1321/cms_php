701df8ccaf3b54c6158a20cb0142e1399f153fb7
arjun1321 <guptaarjun152@gmail.com>
2016-08-16 15:33:28 +0530

    Deleting categories

--- admin/categories.php
+++ admin/categories.php
@@ -65,16 +65,7 @@
                    
                          <div class="col-xs-6">
                             
-                            
-                            
-<?php 
-                    
-                    
-    $query = "SELECT * FROM categories" ;
-    $select_categories = mysqli_query($connection, $query);
-        
-                    
-?>
+      
                              
                              <table class="table table-bordered table-hover ">
                                  <thead>
@@ -88,19 +79,41 @@
                                    
                                     
                                     
-<?php
-                                
+<?php 
+                                     
+    // FIND ALL CATEGORIES QUERY
+      
+     $query = "SELECT * FROM categories" ;
+     $select_categories = mysqli_query($connection, $query);
                                 
      while($row = mysqli_fetch_assoc($select_categories)) {
                         
      $cat_id = $row['cat_id'];
      $cat_title = $row['cat_title'];
          
-     echo "<tr><td>{$cat_id}</td>";
-     echo "<td>{$cat_title}</td></tr>";
+     echo "<tr>";
+     echo "<td>{$cat_id}</td>";
+     echo "<td>{$cat_title}</td>";
+     echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+     echo "</tr>";
     
      }
                                 
+?>
+                                   
+<?php 
+    
+    // DELETE QUERY
+
+    if(isset($_GET['delete'])) {
+        
+        $the_cat_id = $_GET['delete'];
+        
+        $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
+        
+        $delete_query = mysqli_query($connection, $query);
+        header("Location: categories.php");
+    }
 ?>
                                     
                                  </tbody>
--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,19 +1,21 @@
 <?php include "../includes/db.php" ?>
 
+
 <?php ob_start(); ?>
 
 
 
 
-<?php session_start(); ?>
+<?php //session_start(); ?>
 
 <?php 
 
-    if(!isset($_SESSION['user_role'])) {
-        
-        header("Location: ../index.php ");
-    }
+//    if(!isset($_SESSION['user_role'])) {
+//        
+//        header("Location: ../index.php ");
+//    }
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
