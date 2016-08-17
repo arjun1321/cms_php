844198ca3716afa951c2967100741c0c02596041
arjun1321 <guptaarjun152@gmail.com>
2016-08-17 12:29:12 +0530

    Creating a html table in admin to display a list of posts

--- admin/includes/admin_navigations.php
+++ admin/includes/admin_navigations.php
@@ -47,7 +47,7 @@
                         <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
                         <ul id="posts_dropdown" class="collapse">
                             <li>
-                                <a href="#">View All Posts</a>
+                                <a href="../posts.php">View All Posts</a>
                             </li>
                             <li>
                                 <a href="#">Add Posts</a>
@@ -56,7 +56,7 @@
                     </li>
                     
                     <li>
-                        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+                        <a href="../categories.php"><i class="fa fa-fw fa-wrench"></i> Categories</a>
                     </li>
                     <li>
                         <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
---
+++ admin/posts.php
@@ -0,0 +1,68 @@
+<?php include "includes/admin_header.php" ?>
+<?php include "functions.php" ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "includes/admin_navigations.php" ?>   
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                         
+                         <h1 class="page-header">
+                            Welcome to admin
+                            <small>Author</small>
+                        </h1>
+                        
+                        
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Status</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                </tr>
+                            </thead>
+                            
+                            <tbody>
+                                <tr>
+                                    <td>10</td>
+                                    <td>Arjun</td>
+                                    <td>Bootstrap Frameweork</td>
+                                    <td>Bootstrap</td>
+                                    <td>Status</td>
+                                    <td>Image</td>
+                                    <td>Tags</td>
+                                    <td>Comment</td>
+                                    <td>Date</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        
+                         
+                   
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        
+        
+        
+        <!-- /#page-wrapper -->
+
+    <?php include "includes/admin_footer.php"; ?>
