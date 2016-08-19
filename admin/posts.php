92e76ed809080b37d91c3f24cb1aa0fee9c5143f
arjun1321 <guptaarjun152@gmail.com>
2016-08-19 11:09:21 +0530

    creating html edit form for post page

--- admin/posts.php
+++ admin/posts.php
@@ -37,6 +37,10 @@
                                     include "includes/view_all_post.php";
                                     break;
                                     
+                                case 'edit_post':
+                                    include "includes/edit_post.php";
+                                    break;
+                                    
                                 default:
                                     include "includes/view_all_post.php";
                             }
