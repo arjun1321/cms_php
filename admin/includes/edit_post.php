94e509aa4b69977cb050351baea247f9bb33805f
arjun1321 <guptaarjun152@gmail.com>
2016-08-19 11:34:33 +0530

    displaying the edit data in post edit page

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,34 +1,25 @@
 <?php
 
-    if(isset($_POST['create_post'])) {
-        
-        $post_title = $_POST['title'];
-        $post_author = $_POST['author'];
-        $post_category_id = $_POST['post_category_id'];
-        $post_status = $_POST['post_status'];
-        
-        
-        $post_image = $_FILES['image']['name'];
-        $post_image_temp = $_FILES['image']['tmp_name'];
-        
-        $post_tags = $_POST['post_tags'];
-        $post_content = $_POST['post_content'];
-        $post_date = date('d-m-y');
-        $post_comment_count = 4;
-        
-        
-        move_uploaded_file($post_image_temp, "../images/$post_image");
-        
-        
-        $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status) ";
-        
-        $query .= "VALUES({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_image}', '{$post_content}', '{$post_tags}', '{$post_comment_count}', '{$post_status}') ";
-        
-        
-        $create_post_query = mysqli_query($connection, $query);
-        
-        confirm($create_post_query);
-    }
+if(isset($_GET['p_id'])) {
+    
+    $the_post_id = $_GET['p_id'];
+}
+     $query = "SELECT * FROM posts" ;
+     $select_posts_by_id = mysqli_query($connection, $query);
+                                
+     while($row = mysqli_fetch_assoc($select_posts_by_id)) {
+                        
+        $post_id = $row['post_id'];
+        $post_author = $row['post_author'];
+        $post_title = $row['post_title'];
+        $post_category_id = $row['post_category_id'];
+        $post_status = $row['post_status'];
+        $post_image = $row['post_image'];
+        $post_tags = $row['post_tags'];
+        $post_comment_count = $row['post_comment_count'];   
+        $post_date = $row['post_date'];
+        $post_content = $row['post_content'];
+     }
 ?>
   
 
@@ -44,22 +35,22 @@
     
        <div class="form-group">
            <label for="title">Post Title</label>
-           <input type="text" class="form-control" name="title">
+           <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="title">
        </div>
        
     <div class="form-group">
       <label for="post_category">Post Category Id</label> 
-      <input type="text" class="form-control" name="post_category_id"> 
+      <input value="<?php echo $post_category_id; ?>" type="text" class="form-control" name="post_category_id"> 
     </div>
     
     <div class="form-group">
         <label for="title">Post Author</label>
-        <input type="text" class="form-control" name="author">
+        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="author">
     </div>
     
     <div class="form-group">
         <label for="post_status">Post Status</label>
-        <input type="text" class="form-control" name="post_status">
+        <input value="<?php echo $post_status; ?>" type="text" class="form-control" name="post_status">
     </div>
     
     <div class="form-group">
@@ -69,15 +60,17 @@
     
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
-        <input type="text" class="form-control" name="post_tags">
+        <input value="<?php echo $post_tags; ?>" type="text" class="form-control" name="post_tags">
     </div>
     
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea name="post_content" id="" cols="30" rows="10" class="form-control"></textarea>
+        <textarea name="post_content" id="" cols="30" rows="10" class="form-control">
+        <?php echo $post_content; ?>
+        </textarea>
     </div>
     
     <div class="form-group">
-        <input type="submit" class="btn btn-primary" name="create_post" value="Publish Post">
+        <input type="submit" class="btn btn-primary" name="create_post" value="Update Post">
     </div>
 </form>
