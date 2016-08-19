9e71ef54e2234d9a307a871212afdfe15630562e
arjun1321 <guptaarjun152@gmail.com>
2016-08-19 11:03:45 +0530

    deleting post in admin panel

--- admin/includes/view_all_post.php
+++ admin/includes/view_all_post.php
@@ -44,6 +44,7 @@
     echo "<td>$post_tags</td>";
     echo "<td>$post_comment_count</td>";
     echo "<td>$post_date</td>";
+    echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
     echo "</tr>";
 
   }
@@ -52,3 +53,26 @@
                               
                             </tbody>
                         </table>
+                        
+                       
+<?php
+
+   
+
+if(isset($_GET['delete'])) {
+    
+    $the_post_id = $_GET['delete'];
+    
+    $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
+    $delete_query = mysqli_query($connection, $query);
+    
+    header("Location: posts.php");
+}
+?>                    
+                     
+                    
+                   
+                  
+                 
+                
+               
--- admin/posts.php
+++ admin/posts.php
@@ -41,6 +41,9 @@
                                     include "includes/view_all_post.php";
                             }
 
+                            
+                            
+
 
                         ?>
                         
