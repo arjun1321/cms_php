ffb9867ecc336f3f3cf29f5bafb05a66af84bc8a
arjun1321 <guptaarjun152@gmail.com>
2016-08-22 21:16:28 +0530

    adding category dropdown to the add post page

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -4,7 +4,7 @@
         
         $post_title = $_POST['title'];
         $post_author = $_POST['author'];
-        $post_category_id = $_POST['post_category_id'];
+        $post_category_id = $_POST['post_category'];
         $post_status = $_POST['post_status'];
         
         
@@ -48,8 +48,31 @@
        </div>
        
     <div class="form-group">
-      <label for="post_category">Post Category Id</label> 
-      <input type="text" class="form-control" name="post_category_id"> 
+      
+       <select name="post_category" id="">
+           
+           <?php
+           
+            $query = "SELECT * FROM categories";
+            $select_categories = mysqli_query($connection, $query);
+           
+            confirm($select_categories);
+                                
+            while($row = mysqli_fetch_assoc($select_categories)) {
+                        
+            $cat_id = $row['cat_id'];
+            $cat_title = $row['cat_title'];
+                
+                
+            echo "<option value='$cat_id'>{$cat_title}</option>";
+                
+                
+            }
+           ?>
+           
+       </select>
+        
+          
     </div>
     
     <div class="form-group">
