469eb8ae8766a337bc1194cec5406d643ccee06f
arjun1321 <guptaarjun152@gmail.com>
2016-08-27 11:23:34 +0530

    creating the query for displaying comments in admin part 2

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -55,13 +55,12 @@
          
     echo "<td>$comment_email</td>";
     echo "<td>$comment_status</td>";
+    echo "<td>some title</td>";    
     echo "<td>$comment_date</td>";
-    echo "<td>$post_date</td>";
-    echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Approve</a></td>";
-    echo "<td><a href='posts.php?delete={$post_id}'>Unapprove</a></td>";
+    echo "<td><a href='posts.php?source=edit_post&p_id='>Approve</a></td>";
+    echo "<td><a href='posts.php?delete='>Unapprove</a></td>";
          
-    echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
-    echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
+    echo "<td><a href='posts.php?delete='>Delete</a></td>";
     echo "</tr>";
 
   }
