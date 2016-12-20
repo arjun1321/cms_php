8fdfa57c121cbc603d95e05aed533a0f5071cacd
arjun1321 <guptaarjun152@gmail.com>
2016-12-20 13:11:47 +0530

    bug fix

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -9,7 +9,7 @@
 
     if(isset($_SESSION['user_role'])) {
         
-        if($_SESSION['user_role' !== 'admin']) {
+        if($_SESSION['user_role'] !== 'admin') {
         
             header("Location: ../index.php ");
             
