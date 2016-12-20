8b2eb13e0b2218bef7c1b5ae97f0bc60c98ea562
arjun1321 <guptaarjun152@gmail.com>
2016-12-20 11:50:37 +0530

    making the login page

---
+++ includes/login.php
@@ -0,0 +1,13 @@
+<?php include "db.php"; ?>
+
+<?php
+
+    if(isset($_POST['login'])) {
+        
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        
+    }
+    
+    
+?>
