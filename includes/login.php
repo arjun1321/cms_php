b942f653030779eceaf550ea876176b7fedf7004
arjun1321 <guptaarjun152@gmail.com>
2016-12-20 12:12:46 +0530

    validating user query front end

--- includes/login.php
+++ includes/login.php
@@ -19,9 +19,25 @@
         
         while($row = mysqli_fetch_array($select_user_query)) {
             
-            $db_id = $row['user_id'];
+            $db_user_id = $row['user_id'];
+            $db_username = $row['username'];
+            $db_user_password = $row['user_password'];
+            $db_user_firstname = $row['user_firstname'];
+            $db_user_lastname = $row['user_lastname'];
+            $db_user_role = $row['user_role'];
         }
         
+        
+        if($username !== $db_username && $password !== $db_user_password) {
+            
+            header("Location: ../index.php ");
+        } else if($username == $db_username && $password == $db_user_password) {
+            
+            header("Location: ../admin");
+        } else {
+            
+            header("Location: ../index.php");
+        }
         
         
     }
