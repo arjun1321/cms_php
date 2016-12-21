de2dc5e753c9758599a23887c695b0f37f3b30b2
arjun1321 <guptaarjun152@gmail.com>
2016-12-21 19:35:16 +0530

    creating the profile page

--- admin/index.php
+++ admin/index.php
@@ -23,7 +23,7 @@
                     <div class="col-lg-12">
                         <h1 class="page-header">
                             Welcome to admin
-                            
+                             
                             
                             <small><?php echo $_SESSION['username']; ?></small>
                         </h1>
---
+++ admin/profile.php
@@ -0,0 +1,113 @@
+<?php include "includes/admin_header.php" ?>
+<?php include "functions.php" ?>
+    
+    <?php
+        
+        if(isset($_SESSION['username'])) {
+            
+            
+        }
+
+    ?>
+    
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "includes/admin_navigations.php" ?>   
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                         
+                         <h1 class="page-header">
+                            Welcome to admin
+                            <small>Author</small>
+                        </h1>
+                        
+                         
+                         <form action="" method="post" enctype="multipart/form-data">
+   
+
+    <div class="form-group">
+        <label for="title">Firstname</label>
+        <input type="text" value="<?php echo $user_firstname; ?>" class="form-control" name="user_firstname">
+    </div>
+    
+    <div class="form-group">
+        <label for="post_status">Lastname</label>
+        <input type="text" value="<?php echo $user_lastname; ?>" class="form-control" name="user_lastname">
+    </div>
+       
+    <div class="form-group">
+      
+       <select name="user_role" id="">
+ 
+           <option value="subscriber" ><?php echo $user_role; ?></option>
+           
+           <?php
+           
+                if($user_role == 'admin') {
+                    echo "<option value='subscriber'>subscriber</option>";
+                } else {
+                    
+                    echo "<option value='admin'>admin</option>";
+                }
+           ?>
+           
+           
+           
+           
+       </select>
+        
+          
+    </div>
+    
+    
+    
+<!--
+    <div class="form-group">
+        <label for="post_image">Post Image</label>
+        <input type="file" name="image">
+    </div>
+-->
+    
+    <div class="form-group">
+        <label for="post_tags">Username</label>
+        <input type="text" value="<?php echo $username; ?>" class="form-control" name="username">
+    </div>
+    
+    <div class="form-group">
+        <label for="post_content">Email</label>
+        <input type="email" value="<?php echo $user_email; ?>" class="form-control" name="user_email">
+    </div>
+    
+    <div class="form-group">
+        <label for="post_content">Password</label>
+        <input type="password" value="<?php echo $user_password; ?>" class="form-control" name="user_password">
+    </div>
+    
+    <div class="form-group">
+        <input type="submit" class="btn btn-primary" name="edit_user" value="Update User">
+    </div>
+</form>
+                         
+                   
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        
+        
+        
+        <!-- /#page-wrapper -->
+
+    <?php include "includes/admin_footer.php"; ?>
