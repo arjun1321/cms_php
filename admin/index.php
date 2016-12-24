fbf5fc29ec9f8658ebf77d53abbfcdfc56078ad6
arjun1321 <guptaarjun152@gmail.com>
2016-12-24 17:24:26 +0530

    post dynamic data in widget

--- admin/index.php
+++ admin/index.php
@@ -44,7 +44,14 @@
                                         <i class="fa fa-file-text fa-5x"></i>
                                     </div>
                                     <div class="col-xs-9 text-right">
-                                        <div class="huge">20</div>
+                                       
+<?php
+    $query = "SELECT * FROM posts";
+    $select_all_post = mysqli_query($connection, $query);
+    $post_counts = mysqli_num_rows($select_all_post);
+    echo "<div class='huge'>{$post_counts}</div>";
+?>          
+                                        
                                         <div>Posts</div>
                                     </div>
                                 </div>
