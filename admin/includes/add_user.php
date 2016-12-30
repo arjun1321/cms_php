904ccf247377479d1eaa8076ae2a5dcaacf702ac
arjun1321 <guptaarjun152@gmail.com>
2016-12-30 16:29:06 +0530

    added notifications to add user page

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -29,6 +29,8 @@
         $create_user_query = mysqli_query($connection, $query);
         
         confirm($create_user_query);
+        
+        echo "User Created: " . " " . "<a href='users.php'>View Users</a> ";
     }
 ?>
   
