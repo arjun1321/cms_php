cad1bc04fc1c1b55d83ef2cf2b5b56138343575a
arjun1321 <guptaarjun152@gmail.com>
2016-12-31 15:08:42 +0530

    Added dynamic category selection for edit post page

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -125,9 +125,29 @@ if(isset($_GET['p_id'])) {
     </div>
     
     <div class="form-group">
+        <select name="post_status" id="">
+            <option value='<?php echo $post_status; ?>'><?php echo $post_status; ?></option>
+            
+            <?php
+            
+                if($post_status == 'published') {
+                    echo "<option value='draft'>Draft</option>";
+                } else {
+                    echo "<option value='published'>Publish</option>";
+                }
+            ?>
+            
+        </select>
+    </div>
+    
+    
+    
+<!--
+    <div class="form-group">
         <label for="post_status">Post Status</label>
         <input value="<?php echo $post_status; ?>" type="text" class="form-control" name="post_status">
     </div>
+-->
     
     <div class="form-group">
         <img width="100" src="../images/<?php echo $post_image; ?>">
