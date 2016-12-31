0dcdef500c6940c04a2af987d1288a0b96a32135
arjun1321 <guptaarjun152@gmail.com>
2016-12-31 16:02:08 +0530

    added post update notifications

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -70,7 +70,7 @@ if(isset($_GET['p_id'])) {
         
         confirm($update_post);
         
-        
+        echo "<p class='bg-success'>Post Updated. <a href='../post.php?p_id=$the_post_id'>View Post</a> or <a href='posts.php'>Edit More Posts</a></p>";
     }
 
 ?>
