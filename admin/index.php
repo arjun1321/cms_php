cd3fdb23fb14e17f74b8221d5fe37d1c507b97d4
arjun1321 <guptaarjun152@gmail.com>
2016-12-31 15:17:13 +0530

    added extra element to dashboard

--- admin/index.php
+++ admin/index.php
@@ -151,6 +151,11 @@
                 
                 <?php
                 
+                    $query = "SELECT * FROM posts WHERE post_status = 'published'";
+                    $select_all_published_posts = mysqli_query($connection, $query);
+                    $post_published_count = mysqli_num_rows($select_all_published_posts);
+                
+                
                     $query = "SELECT * FROM posts WHERE post_status = 'draft'";
                     $select_all_draft_posts = mysqli_query($connection, $query);
                     $post_draft_count = mysqli_num_rows($select_all_draft_posts);
@@ -178,11 +183,11 @@
                             
                             <?php
                                 
-                                $element_text = ['Active Posts', 'Draft Posts', 'Comments', 'Pending Comments', 'Users', 'Subscribers', 'Categories'];
+                                $element_text = ['All Posts', 'Active Posts', 'Draft Posts', 'Comments', 'Pending Comments', 'Users', 'Subscribers', 'Categories'];
                             
-                                $element_count = [$post_counts, $post_draft_count, $comment_counts, $unapproved_comment_count, $user_counts, $subscriber_count, $categorie_counts];
+                                $element_count = [$post_counts, $post_published_count, $post_draft_count, $comment_counts, $unapproved_comment_count, $user_counts, $subscriber_count, $categorie_counts];
                             
-                                for($i = 0; $i < 7; $i++) {
+                                for($i = 0; $i < 8; $i++) {
                                     
                                     echo "['{$element_text[$i]}'" . "," . "{$element_count[$i]}],";
                                 }
