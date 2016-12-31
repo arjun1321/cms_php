70e3ddf4afd1a1dae6ff9bf5d311046c5e89c1df
arjun1321 <guptaarjun152@gmail.com>
2016-12-31 16:55:46 +0530

    added post edit link in front end

--- includes/navigation.php
+++ includes/navigation.php
@@ -35,14 +35,20 @@
                     <li>
                         <a href="admin">Admin</a>
                     </li>
-<!--
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
--->
+                    
+                 <?php
+                    
+                    if(isset($_SESSION['user_role'])) {
+                        
+                        if(isset($_GET['p_id'])) {
+                            $the_post_id = $_GET['p_id'];
+                            
+                            echo "<li><a href='admin/posts.php?source=edit_post&p_id={$the_post_id}'>Edit Post</a></li>";
+                        }
+                    }
+                    
+                 ?>   
+
 
                     
                     
